45f59ae13d9e7f457437d74e91049d9404236478
dr-js <dr@dr.run>
2019-09-07 21:02:43 +0800

    1mb@0.0.5-dev.1
    notable change:
    - break: require Node 12
    - ci: move to GitHub Actions from Travis CI
    - package update

--- README.md
+++ README.md
@@ -7,10 +7,10 @@
 
 1MB of pure dependency
 
-[i:npm]: https://img.shields.io/npm/v/1mb.svg
-[i:npm-dev]: https://img.shields.io/npm/v/1mb/dev.svg
+[i:npm]: https://img.shields.io/npm/v/1mb
+[i:npm-dev]: https://img.shields.io/npm/v/1mb/dev
 [l:npm]: https://npm.im/1mb
-[i:ci]: https://img.shields.io/travis/dr-js/1mb/master.svg
-[l:ci]: https://travis-ci.org/dr-js/1mb
+[i:ci]: https://img.shields.io/github/workflow/status/dr-js/1mb/ci-test
+[l:ci]: https://github.com/dr-js/1mb/actions?query=workflow:ci-test
 [i:size]: https://packagephobia.now.sh/badge?p=1mb
 [l:size]: https://packagephobia.now.sh/result?p=1mb
--- script/index.js
+++ script/index.js
@@ -1,20 +1,23 @@
 import { resolve } from 'path'
 import { execSync } from 'child_process'
 
-import { runMain, argvFlag } from 'dr-dev/module/main'
-import { initOutput, packOutput, publishOutput } from 'dr-dev/module/output'
+import { initOutput, packOutput, publishOutput } from '@dr-js/dev/module/output'
+import { runMain, argvFlag } from '@dr-js/dev/module/main'
 
 const PATH_ROOT = resolve(__dirname, '..')
 const PATH_OUTPUT = resolve(__dirname, '../output-gitignore')
 const fromRoot = (...args) => resolve(PATH_ROOT, ...args)
 const fromOutput = (...args) => resolve(PATH_OUTPUT, ...args)
-const execOptionRoot = { cwd: fromRoot(), stdio: argvFlag('quiet') ? [ 'ignore', 'ignore', 'inherit' ] : 'inherit', shell: true }
+const execShell = (command) => execSync(command, { cwd: fromRoot(), stdio: argvFlag('quiet') ? [ 'ignore', 'ignore', 'inherit' ] : 'inherit' })
 
 runMain(async (logger) => {
   const isTest = argvFlag('test', 'publish', 'publish-dev')
-  const packageJSON = await initOutput({ copyPathList: [ 'README.md', '1mb.js' ], fromRoot, fromOutput, logger })
+  const packageJSON = await initOutput({
+    copyPathList: [ 'README.md', '1mb.js' ],
+    fromRoot, fromOutput, logger
+  })
   const pathPackagePack = await packOutput({ fromRoot, fromOutput, logger })
-  isTest && execSync('npm run lint', execOptionRoot)
-  isTest && execSync('npm run test-source', execOptionRoot)
+  isTest && execShell('npm run lint')
+  isTest && execShell('npm run test-source')
   await publishOutput({ flagList: process.argv, packageJSON, pathPackagePack, logger })
 })
