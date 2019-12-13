a035d0a1c45f1ef2d6efdd79296a8acecb663725
dr-js <dr@dr.run>
2019-12-13 11:57:05 +0800

    1mb@0.0.5-dev.2
    notable change:
    - sort build script
    - package update

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { execSync } from 'child_process'
 
-import { initOutput, packOutput, publishOutput } from '@dr-js/dev/module/output'
+import { initOutput, packOutput, clearOutput, verifyGitStatusClean, publishOutput } from '@dr-js/dev/module/output'
 import { runMain, argvFlag } from '@dr-js/dev/module/main'
 
 const PATH_ROOT = resolve(__dirname, '..')
@@ -11,13 +11,18 @@ const fromOutput = (...args) => resolve(PATH_OUTPUT, ...args)
 const execShell = (command) => execSync(command, { cwd: fromRoot(), stdio: argvFlag('quiet') ? [ 'ignore', 'ignore', 'inherit' ] : 'inherit' })
 
 runMain(async (logger) => {
-  const isTest = argvFlag('test', 'publish', 'publish-dev')
   const packageJSON = await initOutput({
     copyPathList: [ 'README.md', '1mb.js' ],
     fromRoot, fromOutput, logger
   })
-  const pathPackagePack = await packOutput({ fromRoot, fromOutput, logger })
+  if (!argvFlag('pack')) return
+  const isTest = argvFlag('test', 'publish', 'publish-dev')
+  isTest && logger.padLog('lint source')
   isTest && execShell('npm run lint')
+  isTest && logger.padLog('test source')
   isTest && execShell('npm run test-source')
+  await clearOutput({ fromOutput, logger })
+  isTest && await verifyGitStatusClean({ fromRoot, logger })
+  const pathPackagePack = await packOutput({ fromRoot, fromOutput, logger })
   await publishOutput({ flagList: process.argv, packageJSON, pathPackagePack, logger })
 })
